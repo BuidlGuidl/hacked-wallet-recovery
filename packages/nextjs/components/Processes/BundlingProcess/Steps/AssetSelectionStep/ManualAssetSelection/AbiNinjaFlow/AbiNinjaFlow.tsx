eb9b334a512897bb9cec1801d3c11844bf7806a4
Shiv Bhonde <shivbhonde04@gmail.com>
2024-04-06 21:50:55 +0530

    remove add btn and close modal as soon as we get latestTransaction

--- packages/nextjs/components/Processes/BundlingProcess/Steps/AssetSelectionStep/ManualAssetSelection/AbiNinjaFlow/AbiNinjaFlow.tsx
+++ packages/nextjs/components/Processes/BundlingProcess/Steps/AssetSelectionStep/ManualAssetSelection/AbiNinjaFlow/AbiNinjaFlow.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { ImpersonatorIframe, useImpersonatorIframe } from "@impersonator/iframe";
 import { useReadLocalStorage } from "usehooks-ts";
 import { mainnet } from "viem/chains";
@@ -13,10 +14,47 @@ export const AbiNinjaFlow = ({ addUnsignedTx }: { addUnsignedTx: (asset: IWrappe
   const hackedWalletAddress = useReadLocalStorage<string>("hackedAddress");
   const { latestTransaction } = useImpersonatorIframe();
 
+  const isLatestTransactionPresent = Boolean(latestTransaction);
+  useEffect(() => {
+    const createCustomTx = async () => {
+      try {
+        // @ts-expect-error
+        const contractAddress = latestTransaction.to;
+        // @ts-expect-error
+        const data = latestTransaction.data;
+        // @ts-expect-error
+        const value = latestTransaction.value;
+
+        const customTx: CustomTx = {
+          type: "custom-abininja",
+          info: `Custom abininja call to ${contractAddress} with data ${data}`,
+          toEstimate: {
+            from: hackedWalletAddress as `0x${string}`,
+            to: contractAddress,
+            data: data,
+            value: value,
+          },
+        };
+
+        addUnsignedTx({ tx: customTx });
+        notification.success("Custom transaction added to the list");
+      } catch (e: any) {
+        const message = getParsedError(e);
+        notification.error(message);
+        console.error(e);
+      }
+    };
+
+    if (isLatestTransactionPresent) {
+      createCustomTx();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLatestTransactionPresent, hackedWalletAddress]);
+
   return (
     <div className="flex flex-col gap-1">
       {hackedWalletAddress && (
-        <div className="flex items-center flex-col flex-grow p-4 rounded-md h-[440px]">
+        <div className="flex items-center flex-col flex-grow p-4 rounded-md h-[480px]">
           <div className="border-2 border-gray-200 rounded-md w-full h-full">
             <div className="w-full rounded-md p-1 h-full">
               <ImpersonatorIframe
@@ -31,42 +69,6 @@ export const AbiNinjaFlow = ({ addUnsignedTx }: { addUnsignedTx: (asset: IWrappe
           </div>
         </div>
       )}
-      <button
-        disabled={!Boolean(latestTransaction)}
-        className="btn btn-secondary btn-sm"
-        onClick={async () => {
-          try {
-            if (!latestTransaction) {
-              return;
-            }
-            // @ts-expect-error
-            const contractAddress = latestTransaction.to;
-            // @ts-expect-error
-            const data = latestTransaction.data;
-            // @ts-expect-error
-            const value = latestTransaction.value;
-
-            const customTx: CustomTx = {
-              type: "custom-abininja",
-              info: `Custom abininja call to ${contractAddress} with data ${data}`,
-              toEstimate: {
-                from: hackedWalletAddress as `0x${string}`,
-                to: contractAddress,
-                data: data,
-                value: value,
-              },
-            };
-
-            addUnsignedTx({ tx: customTx });
-          } catch (e: any) {
-            const message = getParsedError(e);
-            notification.error(message);
-            console.error(e);
-          }
-        }}
-      >
-        add
-      </button>
     </div>
   );
 };
