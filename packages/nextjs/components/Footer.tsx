359971d4c538ef1ff195a2206382c18c29b5a4b5
escottalexander@gmail.com <escottalexander@gmail.com>
2024-06-24 14:08:38 -0400

    add source and BG attribution to footer

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -6,15 +6,18 @@ import { SwitchTheme } from "~~/components/SwitchTheme";
  */
 export const Footer = () => {
   return (
-    <div className="min-h-0 p-5 lg:mb-0">
+    <div className="min-h-0 p-2 lg:mb-0">
       <div className="fixed flex justify-between items-center w-full z-20 p-4 bottom-0 left-0 pointer-events-none">
         <SwitchTheme className="pointer-events-auto" />
       </div>
 
-      <div className="w-full">
+      <div className="w-full relative">
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
-            Built with <HeartIcon className="inline-block h-4 w-4" /> at 🏰{" "}
+            <a className="link" href="https://github.com/BuidlGuidl/flashbot-recovery-bundler">
+              Fork me
+            </a>
+            {" · "}Built with <HeartIcon className="inline-block h-4 w-4" /> at 🏰{" "}
             <a href="https://buidlguidl.com/" target="_blank" rel="noreferrer" className="underline underline-offset-2">
               BuidlGuidl
             </a>
