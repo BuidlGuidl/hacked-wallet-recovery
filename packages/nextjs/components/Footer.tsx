19c410654e71f649e522d85afa047c119cfb7544
escottalexander@gmail.com <escottalexander@gmail.com>
2024-06-24 14:16:56 -0400

    open source in new window

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -6,15 +6,20 @@ import { SwitchTheme } from "~~/components/SwitchTheme";
  */
 export const Footer = () => {
   return (
-    <div className="min-h-0 p-2 lg:mb-0">
+    <div className="min-h-0 p-5 lg:mb-0">
       <div className="fixed flex justify-between items-center w-full z-20 p-4 bottom-0 left-0 pointer-events-none">
         <SwitchTheme className="pointer-events-auto" />
       </div>
 
-      <div className="w-full relative">
+      <div className="w-full">
         <ul className="menu menu-horizontal w-full">
           <div className="flex justify-center items-center gap-2 text-sm w-full">
-            <a className="link" href="https://github.com/BuidlGuidl/flashbot-recovery-bundler">
+            <a
+              className="underline underline-offset-2"
+              href="https://github.com/BuidlGuidl/flashbot-recovery-bundler"
+              target="_blank"
+              rel="noreferrer"
+            >
               Fork me
             </a>
             {" · "}Built with <HeartIcon className="inline-block h-4 w-4" /> at 🏰{" "}
