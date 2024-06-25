052a604850911e62394806b38c776787eb1378cf
Kcpele <fidekg123@gmail.com>
2024-06-25 08:40:47 +0100

    fix:text overflow

--- packages/nextjs/components/Processes/BundlingProcess/Steps/AssetSelectionStep/AutoDetectedAssets/AutoDetectedAssetItem.tsx
+++ packages/nextjs/components/Processes/BundlingProcess/Steps/AssetSelectionStep/AutoDetectedAssets/AutoDetectedAssetItem.tsx
@@ -1,8 +1,6 @@
+import ERC20Svg from "../../../../../../public/assets/flashbotRecovery/coin.svg";
 import EmptySvg from "../../../../../../public/assets/flashbotRecovery/empty.svg";
 import TransactionsSvg from "../../../../../../public/assets/flashbotRecovery/transactions.svg";
-
-import ERC20Svg from "../../../../../../public/assets/flashbotRecovery/coin.svg";
-
 import styles from "./autoDetectedAssets.module.css";
 import { motion } from "framer-motion";
 import { RecoveryTx } from "~~/types/business";
@@ -57,21 +55,29 @@ export const AutoDetectedAssetItem = ({ onClick, isSelected, tx, isLoading, imag
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       onClick={onClick}
-      className={`${isSelected ? "bg-base-200" : ""} ${styles.assetItem}  ${isLoading ? styles.loading : ""}`}
+      className={`${isSelected ? "bg-base-200" : ""} ${styles.assetItem}  ${false ? styles.loading : ""}`}
     >
       <div className={`${styles.logoContainer}`}>
         <img
           className={styles.logo}
           width={60}
           height={60}
-          src={image ? image : tx?.type === "erc20" ? ERC20Svg.src : tx?.type ==="custom" ? TransactionsSvg.src :EmptySvg.src}
+          src={
+            image
+              ? image
+              : tx?.type === "erc20"
+              ? ERC20Svg.src
+              : tx?.type === "custom"
+              ? TransactionsSvg.src
+              : EmptySvg.src
+          }
           alt=""
         />
         {tx?.type === "erc20" ? <span className={styles.coinTitle}>ERC20</span> : ""}
       </div>
-      <div className={`${styles.data}`}>
+      <div className={`${styles.data} truncate w-fit`}>
         <h3>{getTitle()}</h3>
-        <span>{getSubtitleTitle()}</span>
+        <span className="overflow-hidden text-ellipsis flex-nowrap">{getSubtitleTitle()}</span>
       </div>
     </motion.div>
   );
